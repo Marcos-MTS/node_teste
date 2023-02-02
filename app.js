244d39f42f838306ee88bf05a5f896770c374a64
Marcos Matthies <marcosmatthies@gmail.com>
2023-02-02 08:25:23 -0300

    Testee

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 //const port = 3000
 
 const PORT = process.env.PORT || 3000
+const BASE_URL = process.env.BASE_URL || "www.teste.com"
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!' + BASE_URL)
 })
 
 app.listen(PORT, () => {
